c03323c3838cba13e017ff62446dcbbb94136a16
Roman <romanfliunt@MacBook-Pro-Roman.local>
2025-09-12 22:45:20 +0300

    hw3

--- src/routers/contactsRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handleGetContacts, handleGetContactById } from '../controllers/contactsControllers.js';
-
-const router = express.Router();
-
-router.get('/', handleGetContacts);
-router.get('/:contactId', handleGetContactById);
-
-export default router;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routers/contactsRouter.js';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
   app.use(pino());
-
   app.use(express.json());
 
- app.use('/contacts', contactsRouter);
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  app.get('/', (req, res) => {
+    res.status(200).send('OK');
   });
+
+  app.use('/contacts', contactsRouter);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   return app;
 };
